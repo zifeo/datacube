afee52dae4f35d3ff5fa3f16894e6057dea0176f
Teo Stocco <teo@zifeo.com>
2018-09-12 21:12:35 -0400

    import react google apps script

---
+++ src/server/index.js
@@ -0,0 +1,12 @@
+import * as publicUiFunctions from './ui';
+import * as publicSheetFunctions from './sheets';
+
+// Expose public functions by attaching to `global`
+global.onOpen = publicUiFunctions.onOpen;
+global.openDialog = publicUiFunctions.openDialog;
+global.openDialogBootstrap = publicUiFunctions.openDialogBootstrap;
+global.openAboutSidebar = publicUiFunctions.openAboutSidebar;
+global.getSheetsData = publicSheetFunctions.getSheetsData;
+global.addSheet = publicSheetFunctions.addSheet;
+global.deleteSheet = publicSheetFunctions.deleteSheet;
+global.setActiveSheet = publicSheetFunctions.setActiveSheet;
